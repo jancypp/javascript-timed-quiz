c204094b8dc597ae2e8ddc78d66caf06fc51a79f
jancypp <120283327+jancypp@users.noreply.github.com>
2023-01-21 15:50:25 -0700

    initial HTML commit and outline of webpage

---
+++ Assets/js/script.js
@@ -0,0 +1,29 @@
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+/**Pseudocode
+ * Prompted to start the quiz by clicking a button - event listener
+ * I will be given a specified amount of time to answer all the questions in the quiz
+ * I will be presented with the first question
+ * I will be presented with the options answers
+ * When I answer correctly ther quiz should move to the next question
+ * When I answer a question incorrectly 10 seconds should be deducted from the timer
+ * I'm alerted if I answered the question correctly or incorrectly
+ * The other questions will not display when the other questions are presented
+ * I want my score tallied and displayed after all the questions were answered
+ * I will be prompted to input my initials when the game is over
+ * I'll be prompted to clear my score or play again
+ */
