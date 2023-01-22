3c6e52121647a2191b880d7af38ab79c790ac230
jancypp <120283327+jancypp@users.noreply.github.com>
2023-01-22 14:45:30 -0700

    added start quiz functionality and created a timer countdown

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,17 +13,92 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+const questions = [
+    {
+        question: "Which tag in the HTML document is used to link a JavaScript file?",
+        options: ["<script>", "<link>", "<src>", "<href>"],
+        answer: "<script>",
+    },
+    {
+        question: "How do you write an array?",
+        options: ["With {}", "With ()", "With <>", "With []"],
+        answer: "With []",
+    },
+    { question: "What is a type of variable in JavaScript?",
+      options: ["None", "Central", "Global", "Target"],
+      answer: "Global",
+    }
+];
 
-/**Pseudocode
- * Prompted to start the quiz by clicking a button - event listener
- * I will be given a specified amount of time to answer all the questions in the quiz
- * I will be presented with the first question
- * I will be presented with the options answers
- * When I answer correctly ther quiz should move to the next question
- * When I answer a question incorrectly 10 seconds should be deducted from the timer
- * I'm alerted if I answered the question correctly or incorrectly
- * The other questions will not display when the other questions are presented
- * I want my score tallied and displayed after all the questions were answered
- * I will be prompted to input my initials when the game is over
- * I'll be prompted to clear my score or play again
- */
+var introSection = document.querySelector(".intro-section"); //row 22 HTML
+var questionSection = document.querySelector(".question-section"); //row 27 HTML
+var questionHeading = document.querySelector("#question-heading"); //row 28 HTML
+var optionOne = document.querySelector("#option1"); //row 30 HTML
+var optionTwo = document.querySelector("#option2"); //row 31 HTML
+var optionThree = document.querySelector("#option3"); //row 32 HTML
+var optionFour = document.querySelector("#option4"); //row 33 HTML
+var initialSection = document.querySelector(".initial-section"); //row 36 HTML
+var resultSection = document.querySelector(".highscore"); // row 43 HTML
+var timerSection = document.querySelector("#timer"); // row 16 HTML
+var timerCount = 60;
+var index = 0;
+var timerInterval;
+
+//Pseudocode
+//  * Prompted to start the quiz by clicking a button - event listener
+var start = document.querySelector("#begin");
+
+start.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  introSection.classList.add("hide");
+  questionSection.classList.remove("hide");
+    runQuestion();
+    startTime();
+  }
+
+  function runQuestion() {
+  questionHeading.textContent = questions[index].question;
+  optionOne.textContent = questions[index].options[0];
+  optionTwo.textContent = questions[index].options[1];
+  optionThree.textContent = questions[index].options[2];
+  optionFour.textContent = questions[index].options[3];
+  }
+
+//  * I will be given a specified amount of time to answer all the questions in the quiz
+function startTime() {
+    // Sets interval in variable
+    timerInterval = setInterval(function() {
+      timerCount--;
+      timerSection.textContent = timerCount;
+      if(timerCount <= 0) {
+        endGame ();
+      }
+    }, 1000);
+  }
+  function endGame(){
+    sendMessage();
+    score = time;
+    clearInterval(timerInterval);
+  }
+
+  function sendMessage (){
+    timerSection.textContent = "Time is up...end of game!"
+  }
+//  * I will be presented with the first question
+//  * I will be presented with the options answers
+//  * When I answer correctly ther quiz should move to the next question
+//  * When I answer a question incorrectly 10 seconds should be deducted from the timer
+//  * I'm alerted if I answered the question correctly or incorrectly
+//  * The other questions will not display when the other questions are presented
+//  * I want my score tallied and displayed after all the questions were answered
+//  * I will be prompted to input my initials when the game is over
+//  * I'll be prompted to clear my score or play again
+//  */
+
+
+
+// introSection.classList.add('hide'); logic for function startQuiz
+//     questionSection.classList.remove('hide');
+
+// var index = 0 not sure what this does
